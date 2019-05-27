20bfe7094c27205ca7abf6a06ab819512c5bc58f
Beck Chen <beck@funkycorp.com>
2019-05-27 12:56:22 +0900

    [Beck] update hsitoryChecker

--- client/src/components/HistoryChecker/HistoryChecker.js
+++ client/src/components/HistoryChecker/HistoryChecker.js
@@ -1,18 +1,11 @@
 import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 
-const HistoryChecker = ({ userInfoComposite, signedInUser, history }) => {
-  const userSignedIn = userInfoComposite.user.id && signedInUser.username;
-  if (history.location.pathname === '/' && userSignedIn) {
+const HistoryChecker = ({ history }) => {
+  if (history.location.pathname === '/') {
     history.push('/main');
   }
 
   return null;
 };
 
-const mapStateToProps = state => ({
-  userInfoComposite: state.user.userInfoComposite,
-  signedInUser: state.user.signedInUser,
-});
-
-export default withRouter(connect(mapStateToProps, null)(HistoryChecker));
+export default withRouter(HistoryChecker);
