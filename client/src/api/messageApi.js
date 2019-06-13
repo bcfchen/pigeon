aee59ec1c711320cb378d659549020b733315ffa
beck chen <beck@funkycorp.com>
2019-06-14 04:57:13 +0900

    [Beck] some comments

--- client/src/api/messageApi.js
+++ client/src/api/messageApi.js
@@ -1,5 +1,20 @@
 import messages from '../data/messages';
 
+/**
+ * In a real-life scenario with live API calls, 
+ * we can handle errors by passing dispatch into these methods 
+ * and dispatching an error handler action when an exception is met
+ * getWithErrorHandling(path, dispatch) {
+    return API.get(this.apiName, path)
+        .then(response => response.data)
+        .catch(err => {
+            const { message } = err;
+            dispatch(addErrorMessage(message));
+            throw (message);
+        });
+}
+ */
+
 export const addMessage = (userId, messageText) => (new Promise((resolve) => {
   setTimeout(() => {
     resolve({
